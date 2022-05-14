41dda37660b51ba8a21bccc5b186b52659e24a55
Azat Ibrakov <azatibrakov@gmail.com>
2022-05-15 01:17:46 +0700

    Implement 'PartialOrd' trait for 'Point' generic struct

--- src/geometries.rs
+++ src/geometries.rs
@@ -1,4 +1,5 @@
 use super::traits;
+use std::cmp::{max, Ordering};
 use std::fmt;
 use std::hash::{Hash, Hasher};
 
@@ -33,6 +34,34 @@ impl<Scalar: PartialEq> PartialEq for Point<Scalar> {
 
     fn ne(&self, other: &Self) -> bool {
         self.0 != other.0 || self.1 != other.1
+    }
+}
+
+impl<Scalar: PartialOrd> PartialOrd for Point<Scalar> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(if self.lt(other) {
+            Ordering::Less
+        } else if self.gt(other) {
+            Ordering::Greater
+        } else {
+            Ordering::Equal
+        })
+    }
+
+    fn ge(&self, other: &Self) -> bool {
+        self.0.ge(&other.0) || self.0.eq(&other.0) && self.1.ge(&other.1)
+    }
+
+    fn gt(&self, other: &Self) -> bool {
+        self.0.gt(&other.0) || self.0.eq(&other.0) && self.1.gt(&other.1)
+    }
+
+    fn le(&self, other: &Self) -> bool {
+        self.0.lt(&other.0) || self.0.eq(&other.0) && self.1.le(&other.1)
+    }
+
+    fn lt(&self, other: &Self) -> bool {
+        self.0.lt(&other.0) || self.0.eq(&other.0) && self.1.lt(&other.1)
     }
 }
 
