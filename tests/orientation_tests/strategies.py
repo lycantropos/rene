b32922982992cc5ae2a443b4caefa4933a8e5fd7
Azat Ibrakov <azatibrakov@gmail.com>
2022-05-15 21:11:27 +0700

    Add test for 'Orientation.__repr__' magic method

---
+++ tests/orientation_tests/strategies.py
@@ -0,0 +1,7 @@
+from hypothesis import strategies
+
+from rene import Orientation
+
+orientations = strategies.sampled_from([
+    Orientation.CLOCKWISE, Orientation.COLLINEAR, Orientation.COUNTERCLOCKWISE
+])
---
+++ tests/orientation_tests/test_repr.py
@@ -0,0 +1,13 @@
+import sys
+
+from hypothesis import given
+
+from rene import Orientation
+from . import strategies
+
+
+@given(strategies.orientations)
+def test_round_trip(orientation: Orientation) -> None:
+    result = repr(orientation)
+
+    assert eval(result, sys.modules) is orientation
