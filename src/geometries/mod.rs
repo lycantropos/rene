968c74240bf20e9114eab00d8a5aa927d5f0d428
Azat Ibrakov <azatibrakov@gmail.com>
2022-05-15 21:58:17 +0700

    Implement 'Hash' trait for 'Contour' generic struct

--- src/geometries/mod.rs
+++ src/geometries/mod.rs
@@ -148,6 +148,33 @@ impl<Scalar: Clone> Contour<Scalar> {
 
 impl<Scalar: Eq> Eq for Contour<Scalar> {}
 
+impl<Scalar: AdditiveGroup + Clone + Hash + MultiplicativeMonoid + Ord + Signed> Hash
+    for Contour<Scalar>
+{
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        let min_vertex_index = self.to_min_vertex_index();
+        self.0[min_vertex_index].hash(state);
+        match self.orientation() {
+            Orientation::Clockwise => {
+                for index in (0..min_vertex_index).rev() {
+                    self.0[index].hash(state);
+                }
+                for index in (min_vertex_index + 1..self.0.len()).rev() {
+                    self.0[index].hash(state);
+                }
+            }
+            _ => {
+                for index in min_vertex_index + 1..self.0.len() {
+                    self.0[index].hash(state);
+                }
+                for index in 0..min_vertex_index {
+                    self.0[index].hash(state);
+                }
+            }
+        }
+    }
+}
+
 fn cross_multiply<Scalar: AdditiveGroup + MultiplicativeMonoid>(
     first_start: Point<Scalar>,
     first_end: Point<Scalar>,
