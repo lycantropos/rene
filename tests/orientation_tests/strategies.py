7cfa799ced56c5a8c897d2d562c1f2087232e2bc
Azat Ibrakov <azatibrakov@gmail.com>
2022-05-15 21:51:58 +0700

    Add test for 'Orientation.__new__' magic method

--- tests/orientation_tests/strategies.py
+++ tests/orientation_tests/strategies.py
@@ -2,6 +2,7 @@ from hypothesis import strategies
 
 from rene import Orientation
 
+orientations_values = strategies.sampled_from([-1, 0, 1])
 orientations = strategies.sampled_from([
     Orientation.CLOCKWISE, Orientation.COLLINEAR, Orientation.COUNTERCLOCKWISE
 ])
