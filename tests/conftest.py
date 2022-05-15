50cafc53199ebd3ca07ee39dc1a805520a8ed481
Azat Ibrakov <azatibrakov@gmail.com>
2022-05-16 00:49:06 +0700

    Complete tests configurations

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,25 +1,18 @@
 import os
 from datetime import timedelta
-from typing import List
 
 import pytest
 from hypothesis import (HealthCheck,
                         settings)
 
-
-@pytest.hookimpl(trylast=True)
-def pytest_collection_modifyitems(session: pytest.Session,
-                                  config: pytest.Config,
-                                  items: List[pytest.Item]) -> None:
-    on_ci = bool(os.getenv('CI', False))
-    max_examples = settings.default.max_examples
-    settings.register_profile('default',
-                              deadline=((timedelta(hours=1)
-                                         / (max_examples * len(items)))
-                                        if items and on_ci
-                                        else None),
-                              max_examples=max_examples,
-                              suppress_health_check=[HealthCheck.too_slow])
+on_ci = bool(os.getenv('CI', False))
+max_examples = settings.default.max_examples
+settings.register_profile('default',
+                          deadline=(timedelta(hours=1) / max_examples
+                                    if on_ci
+                                    else None),
+                          max_examples=max_examples,
+                          suppress_health_check=[HealthCheck.too_slow])
 
 
 @pytest.hookimpl(trylast=True)
