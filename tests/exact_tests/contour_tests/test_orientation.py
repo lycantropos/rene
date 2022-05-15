adba216532c872e0049cb163395177d4373185b1
Azat Ibrakov <azatibrakov@gmail.com>
2022-05-15 21:47:24 +0700

    Add test for 'Contour.orientation' property

---
+++ tests/exact_tests/contour_tests/test_orientation.py
@@ -0,0 +1,16 @@
+from hypothesis import given
+
+from rene import Orientation
+from rene.exact import Contour
+from tests.utils import rotate_contour
+from . import strategies
+
+
+@given(strategies.contours)
+def test_basic(contour: Contour) -> None:
+    assert isinstance(contour.orientation, Orientation)
+
+
+@given(strategies.contours, strategies.non_zero_integers)
+def test_vertices_rotations(contour: Contour, offset: int) -> None:
+    assert contour.orientation is rotate_contour(contour, offset).orientation
