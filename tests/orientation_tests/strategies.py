e9c269ae2f50549434d9fcf8679dc370db25648e
Azat Ibrakov <azatibrakov@gmail.com>
2022-05-26 11:05:11 +0700

    Remove test of 'Orientation.__new__' magic method

--- tests/orientation_tests/strategies.py
+++ tests/orientation_tests/strategies.py
@@ -2,7 +2,6 @@ from hypothesis import strategies
 
 from rene import Orientation
 
-orientations_values = strategies.sampled_from([-1, 0, 1])
 orientations = strategies.sampled_from([
     Orientation.CLOCKWISE, Orientation.COLLINEAR, Orientation.COUNTERCLOCKWISE
 ])
