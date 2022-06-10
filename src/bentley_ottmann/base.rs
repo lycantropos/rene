2a3103ec3f76cf9211b2808216fa211fb6216f4e
Azat Ibrakov <azatibrakov@gmail.com>
2022-06-10 15:50:36 +0700

    Add 'is_contour_valid' generic function

--- src/bentley_ottmann/base.rs
+++ src/bentley_ottmann/base.rs
@@ -1,15 +1,50 @@
 use core::convert::From;
-use std::cmp::Ordering;
 
 use rithm::traits::{AdditiveGroup, DivisivePartialMagma, MultiplicativeMonoid, Signed};
 
-use crate::operations::relate_segments;
 use crate::relatable::Relation;
-use crate::traits::{Point, Segment};
+use crate::traits::{Contour, Point, Segment};
 
 use super::event::is_left_event;
 use super::events_registry::EventsRegistry;
-use super::traits::{EventsQueue, SweepLine};
+use super::sweep::Sweep;
+
+pub(crate) fn is_contour_valid<
+    Scalar: AdditiveGroup + Clone + DivisivePartialMagma + MultiplicativeMonoid + Ord + Signed,
+    Endpoint: Clone + From<(Scalar, Scalar)> + Ord + self::Point<Scalar>,
+    Segment: self::Segment<Scalar, Point = Endpoint>,
+    Contour: self::Contour<Scalar, Point = Endpoint, Segment = Segment>,
+>(
+    contour: &Contour,
+) -> bool {
+    let segments = contour.segments();
+    segments.len() >= 3 && {
+        let mut sweep = Sweep::from(segments.as_slice());
+        let intersection = unsafe { sweep.next().unwrap_unchecked() };
+        matches!(intersection.relation, Relation::Touch) && {
+            let mut segment_id = intersection.first_segment_id;
+            let mut has_second_tangent = false;
+            while let Some(intersection) = sweep.next() {
+                if !matches!(intersection.relation, Relation::Touch) {
+                    return false;
+                } else if intersection.first_segment_id == segment_id {
+                    if has_second_tangent {
+                        return false;
+                    }
+                    has_second_tangent = true;
+                } else {
+                    debug_assert_ne!(intersection.second_segment_id, segment_id);
+                    if !has_second_tangent {
+                        return false;
+                    }
+                    segment_id = intersection.first_segment_id;
+                    has_second_tangent = false;
+                }
+            }
+            true
+        }
+    }
+}
 
 pub(crate) fn to_unique_non_crossing_or_overlapping_segments<
     Scalar: AdditiveGroup + Clone + DivisivePartialMagma + MultiplicativeMonoid + Ord + Signed,
