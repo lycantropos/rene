0c90fb9b440fded6bf48de4faccebc48392ab246
Azat Ibrakov <azatibrakov@gmail.com>
2022-06-11 23:47:36 +0700

    Add test for 'Relation.__hash__' magic method

---
+++ tests/relation_tests/test__hash__.py
@@ -0,0 +1,17 @@
+from hypothesis import given
+
+from rene import Relation
+from tests.utils import implication
+from . import strategies
+
+
+@given(strategies.relations)
+def test_determinism(relation: Relation) -> None:
+    result = hash(relation)
+
+    assert result == hash(relation)
+
+
+@given(strategies.relations, strategies.relations)
+def test_preserving_equality(first: Relation, second: Relation) -> None:
+    assert implication(first == second, hash(first) == hash(second))
