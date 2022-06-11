b50aa40fda142544d6c20e18f50713c301fb2d46
Azat Ibrakov <azatibrakov@gmail.com>
2022-06-11 23:46:42 +0700

    Add test for 'Orientation.__hash__' magic method

---
+++ tests/orientation_tests/test__hash__.py
@@ -0,0 +1,17 @@
+from hypothesis import given
+
+from rene import Orientation
+from tests.utils import implication
+from . import strategies
+
+
+@given(strategies.orientations)
+def test_determinism(orientation: Orientation) -> None:
+    result = hash(orientation)
+
+    assert result == hash(orientation)
+
+
+@given(strategies.orientations, strategies.orientations)
+def test_preserving_equality(first: Orientation, second: Orientation) -> None:
+    assert implication(first == second, hash(first) == hash(second))
