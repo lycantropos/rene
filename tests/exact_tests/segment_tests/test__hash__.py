7841289de4ea2986b44a3996f8043a1940303eef
Azat Ibrakov <azatibrakov@gmail.com>
2022-06-11 23:50:47 +0700

    Add test for 'Segment.__hash__' magic method

---
+++ tests/exact_tests/segment_tests/test__hash__.py
@@ -0,0 +1,17 @@
+from hypothesis import given
+
+from rene.exact import Segment
+from tests.utils import implication
+from . import strategies
+
+
+@given(strategies.segments)
+def test_determinism(segment: Segment) -> None:
+    result = hash(segment)
+
+    assert result == hash(segment)
+
+
+@given(strategies.segments, strategies.segments)
+def test_preserving_equality(first: Segment, second: Segment) -> None:
+    assert implication(first == second, hash(first) == hash(second))
