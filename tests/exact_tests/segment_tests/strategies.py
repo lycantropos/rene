553d01baaf4bdca051e62e36b71021d60f8c35fe
Azat Ibrakov <azatibrakov@gmail.com>
2022-06-23 22:24:10 +0700

    Complete strategies

--- tests/exact_tests/segment_tests/strategies.py
+++ tests/exact_tests/segment_tests/strategies.py
@@ -1,17 +1,9 @@
 from hypothesis import strategies
-from rithm import Fraction
 
-from rene.exact import (Point,
-                        Segment)
+from rene.exact import Segment
+from tests.strategies import points
 from tests.utils import pack
 
-integers = strategies.integers()
-non_zero_integers = integers.filter(bool)
-scalars = (integers | strategies.fractions()
-           | strategies.builds(Fraction, integers, non_zero_integers)
-           | strategies.floats(allow_infinity=False,
-                               allow_nan=False))
-points = strategies.builds(Point, scalars, scalars)
 segments_endpoints = strategies.lists(points,
                                       min_size=2,
                                       max_size=2,
