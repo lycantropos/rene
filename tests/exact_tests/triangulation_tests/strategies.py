4e1ef783d49f0087829abe5b18bd87d8ca3f812a
Azat Ibrakov <azatibrakov@gmail.com>
2022-07-02 22:22:40 +0700

    Complete strategies

--- tests/exact_tests/triangulation_tests/strategies.py
+++ tests/exact_tests/triangulation_tests/strategies.py
@@ -2,6 +2,7 @@ from hypothesis import strategies
 
 from tests.strategies import points
 
+points = points
 points_lists = strategies.lists(points,
                                 min_size=1)
 two_or_more_points_lists = strategies.lists(points,
