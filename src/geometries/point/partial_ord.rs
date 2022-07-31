78f2a8665fd85892f24bbd3d820c03345b554890
Azat Ibrakov <azatibrakov@gmail.com>
2022-08-01 01:12:24 +0700

    Complete 'PartialOrd' trait implementation of 'Point' generic struct

--- src/geometries/point/partial_ord.rs
+++ src/geometries/point/partial_ord.rs
@@ -4,28 +4,9 @@ use super::types::Point;
 
 impl<Scalar: PartialOrd> PartialOrd for Point<Scalar> {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(if self.lt(other) {
-            Ordering::Less
-        } else if self.gt(other) {
-            Ordering::Greater
-        } else {
-            Ordering::Equal
-        })
-    }
-
-    fn ge(&self, other: &Self) -> bool {
-        self.x.ge(&other.x) || self.x.eq(&other.x) && self.y.ge(&other.y)
-    }
-
-    fn gt(&self, other: &Self) -> bool {
-        self.x.gt(&other.x) || self.x.eq(&other.x) && self.y.gt(&other.y)
-    }
-
-    fn le(&self, other: &Self) -> bool {
-        self.x.lt(&other.x) || self.x.eq(&other.x) && self.y.le(&other.y)
-    }
-
-    fn lt(&self, other: &Self) -> bool {
-        self.x.lt(&other.x) || self.x.eq(&other.x) && self.y.lt(&other.y)
+        match self.x.partial_cmp(&other.x)? {
+            Ordering::Equal => self.y.partial_cmp(&other.y),
+            ordering => Some(ordering),
+        }
     }
 }
