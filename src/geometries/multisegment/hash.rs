220556582fb997a5e0dd9ff53b4bb5d5913daffc
Azat Ibrakov <azatibrakov@gmail.com>
2022-08-26 15:12:07 +0700

    Complete 'Hash' trait implementation of 'Multisegment' generic struct

--- src/geometries/multisegment/hash.rs
+++ src/geometries/multisegment/hash.rs
@@ -1,11 +1,14 @@
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{BuildHasherDefault, Hash, Hasher};
 
-use crate::geometries::utils;
+use crate::geometries::{utils, Segment};
 
 use super::types::Multisegment;
 
-impl<Scalar: PartialOrd + Hash> Hash for Multisegment<Scalar> {
+impl<Scalar> Hash for Multisegment<Scalar>
+where
+    Segment<Scalar>: Hash,
+{
     fn hash<H: Hasher>(&self, state: &mut H) {
         utils::hash_slice_unordered::<_, H, BuildHasherDefault<DefaultHasher>>(
             &self.segments,
