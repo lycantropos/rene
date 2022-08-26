3f314f52599f2f95cddcfd73a575b894d8d72d18
Azat Ibrakov <azatibrakov@gmail.com>
2022-08-26 15:02:57 +0700

    Complete 'PartialEq' trait implementation of 'Multisegment' generic struct

--- src/geometries/multisegment/partial_eq.rs
+++ src/geometries/multisegment/partial_eq.rs
@@ -1,10 +1,13 @@
 use std::hash::Hash;
 
-use crate::geometries::contracts;
+use crate::geometries::{contracts, Segment};
 
 use super::types::Multisegment;
 
-impl<Scalar: Hash + PartialOrd + Eq> PartialEq for Multisegment<Scalar> {
+impl<Scalar> PartialEq for Multisegment<Scalar>
+where
+    Segment<Scalar>: Hash + Eq,
+{
     fn eq(&self, other: &Self) -> bool {
         contracts::are_unique_hashable_sequences_permutationally_equivalent(
             &self.segments,
