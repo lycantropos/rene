026cc99cde0c2848103cee4908bc6e4226de703b
Azat Ibrakov <azatibrakov@gmail.com>
2022-09-01 22:17:03 +0700

    Add test for 'Box.enclosed_by' method

---
+++ tests/exact_tests/box_tests/test_enclosed_by.py
@@ -0,0 +1,19 @@
+from hypothesis import given
+
+from rene import Relation
+from rene.exact import Box
+from tests.utils import equivalence
+from . import strategies
+
+
+@given(strategies.boxes, strategies.boxes)
+def test_basic(first: Box, second: Box) -> None:
+    result = first.enclosed_by(second)
+
+    assert isinstance(result, bool)
+
+
+@given(strategies.boxes, strategies.boxes)
+def test_alternatives(first: Box, second: Box) -> None:
+    assert equivalence(first.enclosed_by(second),
+                       first.relate_to(second) is Relation.ENCLOSED)
