33a1597d71f328ded6a98465d086739ba2248b9b
Azat Ibrakov <azatibrakov@gmail.com>
2022-09-01 21:30:13 +0700

    Add test for 'Box.touches' method

---
+++ tests/exact_tests/box_tests/test_touches.py
@@ -0,0 +1,19 @@
+from hypothesis import given
+
+from rene import Relation
+from rene.exact import Box
+from tests.utils import equivalence
+from . import strategies
+
+
+@given(strategies.boxes, strategies.boxes)
+def test_basic(first: Box, second: Box) -> None:
+    result = first.touches(second)
+
+    assert isinstance(result, bool)
+
+
+@given(strategies.boxes, strategies.boxes)
+def test_alternatives(first: Box, second: Box) -> None:
+    assert equivalence(first.touches(second),
+                       first.relate_to(second) is Relation.TOUCH)
