8ad937213e4c8c9d7c53090551fa7536ece04b82
Azat Ibrakov <azatibrakov@gmail.com>
2022-09-06 00:01:12 +0700

    Complete 'Difference' generic trait implementation of 'Polygon' generic struct

--- src/geometries/polygon/difference.rs
+++ src/geometries/polygon/difference.rs
@@ -3,11 +3,50 @@ use rithm::fraction::Fraction;
 
 use crate::bounded::{Bounded, Box};
 use crate::clipping::{Event, Operation, ReduceEvents, DIFFERENCE};
-use crate::geometries::Point;
+use crate::geometries::{Empty, Point};
+use crate::operations::are_boxes_uncoupled;
 use crate::relatable::Relatable;
 use crate::traits::{Difference, Elemental};
 
 use super::types::Polygon;
+
+impl<Scalar> Difference<Empty> for Polygon<Scalar> {
+    type Output = Self;
+
+    fn difference(self, _other: Empty) -> Self::Output {
+        self
+    }
+}
+
+impl<Scalar> Difference<&Empty> for Polygon<Scalar> {
+    type Output = Self;
+
+    fn difference(self, _other: &Empty) -> Self::Output {
+        self
+    }
+}
+
+impl<Scalar> Difference<Empty> for &Polygon<Scalar>
+where
+    Polygon<Scalar>: Clone,
+{
+    type Output = Polygon<Scalar>;
+
+    fn difference(self, _other: Empty) -> Self::Output {
+        self.clone()
+    }
+}
+
+impl<Scalar> Difference<&Empty> for &Polygon<Scalar>
+where
+    Polygon<Scalar>: Clone,
+{
+    type Output = Polygon<Scalar>;
+
+    fn difference(self, _other: &Empty) -> Self::Output {
+        self.clone()
+    }
+}
 
 impl<Digit, const SEPARATOR: char, const SHIFT: usize> Difference
     for &Polygon<Fraction<BigInt<Digit, SEPARATOR, SHIFT>>>
@@ -17,6 +56,7 @@ where
             &'a Polygon<Fraction<BigInt<Digit, SEPARATOR, SHIFT>>>,
             &'a Polygon<Fraction<BigInt<Digit, SEPARATOR, SHIFT>>>,
         )> + Iterator<Item = Event>,
+    Fraction<BigInt<Digit, SEPARATOR, SHIFT>>: PartialOrd,
     Point<Fraction<BigInt<Digit, SEPARATOR, SHIFT>>>:
         Elemental<Coordinate = Fraction<BigInt<Digit, SEPARATOR, SHIFT>>>,
     Polygon<Fraction<BigInt<Digit, SEPARATOR, SHIFT>>>: Bounded<Fraction<BigInt<Digit, SEPARATOR, SHIFT>>>
@@ -32,11 +72,10 @@ where
     fn difference(self, other: Self) -> Self::Output {
         let bounding_box = self.to_bounding_box();
         let other_bounding_box = other.to_bounding_box();
-        if bounding_box.disjoint_with(&other_bounding_box)
-            || bounding_box.touches(&other_bounding_box)
-        {
+        if are_boxes_uncoupled(&bounding_box, &other_bounding_box) {
             return vec![self.clone()];
         }
+        let max_x = bounding_box.get_max_x();
         let mut operation = Operation::<Point<_>, DIFFERENCE>::from((self, other));
         let mut events = {
             let (_, maybe_events_count) = operation.size_hint();
@@ -44,6 +83,9 @@ where
             Vec::with_capacity(unsafe { maybe_events_count.unwrap_unchecked() })
         };
         while let Some(event) = operation.next() {
+            if operation.get_event_start(event).x().gt(max_x) {
+                break;
+            }
             events.push(event)
         }
         Polygon::<_>::reduce_events(events, &mut operation)
