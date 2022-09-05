74e7569fab9c44f5f01f3b252f12aa8e60c40628
Azat Ibrakov <azatibrakov@gmail.com>
2022-09-06 01:02:18 +0700

    Complete test of 'ConstrainedDelaunayTriangulation.from_polygon' method

--- tests/exact_tests/constrained_delaunay_triangulation_tests/test_from_polygon.py
+++ tests/exact_tests/constrained_delaunay_triangulation_tests/test_from_polygon.py
@@ -1,13 +1,8 @@
-from functools import reduce
-from operator import or_, xor
-
-from gon import base
 from hypothesis import given
 
 from rene.exact import (ConstrainedDelaunayTriangulation,
                         Polygon)
 from . import strategies
-from ... import to_polygon
 
 
 @given(strategies.polygons)
@@ -22,16 +17,3 @@ def test_border(polygon: Polygon) -> None:
     result = ConstrainedDelaunayTriangulation.from_polygon(polygon)
 
     assert result.border == polygon.border
-
-
-@given(strategies.polygons)
-def test_triangles(polygon: Polygon) -> None:
-    triangulation = ConstrainedDelaunayTriangulation.from_polygon(polygon)
-
-    assert triangulation.border == polygon.border
-    triangles = triangulation.triangles
-    # draw(polygon, triangulation)
-    assert triangles
-    united = to_polygon(reduce(xor, [base.Polygon(border, [])
-                                     for border in triangles]))
-    assert united == polygon
