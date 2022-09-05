8b0391eeae752aa0fb91da90a01bd9b00986df57
Azat Ibrakov <azatibrakov@gmail.com>
2022-09-05 09:30:02 +0700

    Complete test of 'Polygon.__eq__' magic method

--- tests/exact_tests/polygon_tests/test__eq__.py
+++ tests/exact_tests/polygon_tests/test__eq__.py
@@ -3,7 +3,6 @@ from hypothesis import given
 from rene.exact import Polygon
 from tests.utils import (equivalence,
                          implication,
-                         reverse_polygon_border,
                          reverse_polygon_holes,
                          rotate_each_polygon_hole,
                          rotate_polygon_border,
@@ -29,11 +28,6 @@ def test_transitivity(first: Polygon, second: Polygon, third: Polygon) -> None:
 @given(strategies.polygons, strategies.polygons)
 def test_alternatives(first: Polygon, second: Polygon) -> None:
     assert equivalence(first == second, not first != second)
-
-
-@given(strategies.polygons)
-def test_border_reversal(polygon: Polygon) -> None:
-    assert polygon == reverse_polygon_border(polygon)
 
 
 @given(strategies.polygons, strategies.non_zero_integers)
