f9623e59aca921eed32d80348c6ec50a583db982
Azat Ibrakov <azatibrakov@gmail.com>
2022-09-07 05:20:23 +0400

    Complete variables names of 'difference' module

--- rene/_clipping/difference.py
+++ rene/_clipping/difference.py
@@ -50,14 +50,14 @@ def subtract_multipolygons(first: Multipolygon,
         second_polygons[polygon_id]
         for polygon_id in second_common_area_polygons_ids
     ]
-    max_x = max(first_boxes[polygon_id].max_x
-                for polygon_id in first_common_area_polygons_ids)
+    first_max_x = max(first_boxes[polygon_id].max_x
+                      for polygon_id in first_common_area_polygons_ids)
     operation = Difference.from_multisegmentals_sequences(
             first_common_area_polygons, second_common_area_polygons
     )
     events = []
     for event in operation:
-        if operation.to_event_start(event).x > max_x:
+        if operation.to_event_start(event).x > first_max_x:
             break
         events.append(event)
     result = operation.reduce_events(events, type(first_polygons[0].border),
@@ -92,14 +92,14 @@ def subtract_polygon_from_multipolygon(first: Multipolygon,
         first_polygons[polygon_id]
         for polygon_id in first_common_area_polygons_ids
     ]
-    max_x = max(first_boxes[polygon_id].max_x
-                for polygon_id in first_common_area_polygons_ids)
+    first_max_x = max(first_boxes[polygon_id].max_x
+                      for polygon_id in first_common_area_polygons_ids)
     operation = Difference.from_multisegmentals_sequence_multisegmental(
             first_common_area_polygons, second
     )
     events = []
     for event in operation:
-        if operation.to_event_start(event).x > max_x:
+        if operation.to_event_start(event).x > first_max_x:
             break
         events.append(event)
     result = operation.reduce_events(events, type(first_polygons[0].border),
@@ -130,13 +130,13 @@ def subtract_multipolygon_from_polygon(first: Polygon,
         second_polygons[polygon_id]
         for polygon_id in second_common_area_polygons_ids
     ]
-    max_x = first_bounding_box.max_x
+    first_max_x = first_bounding_box.max_x
     operation = Difference.from_multisegmental_multisegmentals_sequence(
             first, second_common_area_polygons
     )
     events = []
     for event in operation:
-        if operation.to_event_start(event).x > max_x:
+        if operation.to_event_start(event).x > first_max_x:
             break
         events.append(event)
     return operation.reduce_events(events, type(first.border), type(first))
@@ -148,10 +148,10 @@ def subtract_polygons(first: Polygon, second: Polygon) -> List[Polygon]:
     if do_boxes_have_no_common_area(first_bounding_box, second_bounding_box):
         return [first]
     operation = Difference.from_multisegmentals(first, second)
-    max_x = first_bounding_box.max_x
+    first_max_x = first_bounding_box.max_x
     events = []
     for event in operation:
-        if operation.to_event_start(event).x > max_x:
+        if operation.to_event_start(event).x > first_max_x:
             break
         events.append(event)
     return operation.reduce_events(events, type(first.border), type(first))
