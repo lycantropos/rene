0e086a60941af06f7e313dfd4320666f7c38c7ee
Azat Ibrakov <azatibrakov@gmail.com>
2022-12-28 16:38:55 +0100

    Add test for 'Segment.relate_to' method

---
+++ tests/exact_tests/segment_tests/test_relate_to.py
@@ -0,0 +1,22 @@
+from hypothesis import given
+
+from rene import Relation
+from rene.exact import Segment
+from tests.utils import reverse_segment_endpoints
+from . import strategies
+
+
+@given(strategies.segments, strategies.segments)
+def test_basic(first: Segment, second: Segment) -> None:
+    result = first.relate_to(second)
+
+    assert isinstance(result, Relation)
+
+
+@given(strategies.segments, strategies.segments)
+def test_reversals(first: Segment, second: Segment) -> None:
+    assert (first.relate_to(second)
+            is reverse_segment_endpoints(first).relate_to(
+                    reverse_segment_endpoints(second)
+            ))
+    assert first.relate_to(second) is second.relate_to(first).complement
