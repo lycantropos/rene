498d3f6a642f576768bb5d98caa982b0f833cb12
Azat Ibrakov <azatibrakov@gmail.com>
2023-03-21 23:10:36 +0100

    Complete 'Relatable' trait implementation of 'Segment' struct

--- src/geometries/segment/relatable.rs
+++ src/geometries/segment/relatable.rs
@@ -8,6 +8,11 @@ impl<Scalar> Relatable for &Segment<Scalar>
 where
     Point<Scalar>: Orient + PartialOrd,
 {
+    fn equals_to(self, other: Self) -> bool {
+        self.start.eq(&other.start) && self.end.eq(&other.end)
+            || self.start.eq(&other.end) && self.end.eq(&other.start)
+    }
+
     fn relate_to(self, other: Self) -> Relation {
         segment_in_segment(&self.start, &self.end, &other.start, &other.end)
     }
