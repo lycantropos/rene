57e9c4db96647c9e78a714e865eb87c0bfbf3195
Azat Ibrakov <azatibrakov@gmail.com>
2023-03-28 03:15:34 +0200

    Complete project metadata

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ from pathlib import Path
 from setuptools import (find_packages,
                         setup)
 
-import rene
-
 project_base_url = 'https://github.com/lycantropos/rene/'
 
 
@@ -13,53 +11,14 @@ def read_file(path_string: str) -> str:
     return Path(path_string).read_text(encoding='utf-8')
 
 
-parameters = dict(
-        name=rene.__name__,
-        packages=find_packages(exclude=('tests', 'tests.*')),
-        package_data={
-            rene.__name__: ['__init__.pyi', 'exact.pyi', 'py.typed']
-        },
-        version=rene.__version__,
-        description=rene.__doc__,
-        long_description=read_file('README.md'),
-        long_description_content_type='text/markdown',
-        author='Azat Ibrakov',
-        author_email='azatibrakov@gmail.com',
-        license='MIT License',
-        classifiers=[
-            'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
-            'Programming Language :: Python :: 3.9',
-            'Programming Language :: Python :: 3.10',
-            'Programming Language :: Python :: 3.11',
-            'Programming Language :: Python :: Implementation :: CPython',
-            'Programming Language :: Python :: Implementation :: PyPy',
-        ],
-        url=project_base_url,
-        download_url=project_base_url + 'archive/master.zip',
-        python_requires='>=3.7',
-        install_requires=read_file('requirements.txt'),
-        setup_requires=read_file('requirements-setup.txt'))
+parameters = dict(packages=find_packages(exclude=('tests', 'tests.*')),
+                  url=project_base_url,
+                  download_url=project_base_url + 'archive/master.zip')
 if platform.python_implementation() == 'CPython':
-    from typing import (TYPE_CHECKING,
-                        Iterator)
+    from setuptools_rust import RustExtension
 
-    if TYPE_CHECKING:
-        from setuptools_rust import RustExtension
-
-
-    class LazyRustExtensions(list):
-        def __iter__(self) -> Iterator['RustExtension']:
-            from setuptools_rust import RustExtension
-            yield RustExtension(rene.__name__ + '._cexact')
-            yield RustExtension(rene.__name__ + '._crene')
-
-        def __len__(self) -> int:
-            return 2
-
-
-    parameters.update(rust_extensions=LazyRustExtensions(),
+    parameters.update(rust_extensions=[RustExtension('rene._cexact'),
+                                       RustExtension('rene._crene')],
                       include_package_data=True,
                       zip_safe=False)
 setup(**parameters)
