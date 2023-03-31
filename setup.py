b4026989a9917d1e6255b48a7268047df9d434d1
Azat Ibrakov <azatibrakov@gmail.com>
2023-03-31 04:08:42 +0200

    Complete setup configurations

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
 import platform
-from pathlib import Path
 
 from setuptools import (find_packages,
                         setup)
 
 project_base_url = 'https://github.com/lycantropos/rene/'
-
-
-def read_file(path_string: str) -> str:
-    return Path(path_string).read_text(encoding='utf-8')
-
-
 parameters = dict(packages=find_packages(exclude=('tests', 'tests.*')),
                   url=project_base_url,
                   download_url=project_base_url + 'archive/master.zip')
@@ -19,6 +12,5 @@ if platform.python_implementation() == 'CPython':
 
     parameters.update(rust_extensions=[RustExtension('rene._cexact'),
                                        RustExtension('rene._crene')],
-                      include_package_data=True,
                       zip_safe=False)
 setup(**parameters)
