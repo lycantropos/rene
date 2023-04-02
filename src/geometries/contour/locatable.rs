9622fe8a5a2ccfef25948d6a74cf80775a90b46c
Azat Ibrakov <azatibrakov@gmail.com>
2023-04-02 05:11:16 +0200

    Add 'Locatable' trait implementation for 'Contour' generic struct

---
+++ src/geometries/contour/locatable.rs
@@ -0,0 +1,28 @@
+use crate::geometries::Point;
+use crate::locatable::{Locatable, Location};
+use crate::operations::{is_point_in_segment, Orient};
+use crate::traits::Elemental;
+
+use super::types::Contour;
+
+impl<Scalar: PartialOrd> Locatable<&Point<Scalar>> for &Contour<Scalar>
+where
+    Point<Scalar>: Elemental<Coordinate = Scalar> + Orient + PartialEq,
+{
+    fn locate(self, point: &Point<Scalar>) -> Location {
+        for index in 0..self.vertices.len() - 1 {
+            if is_point_in_segment(&self.vertices[index], &self.vertices[index + 1], point) {
+                return Location::Boundary;
+            }
+        }
+        if is_point_in_segment(
+            &self.vertices[self.vertices.len() - 1],
+            &self.vertices[0],
+            point,
+        ) {
+            Location::Boundary
+        } else {
+            Location::Exterior
+        }
+    }
+}
