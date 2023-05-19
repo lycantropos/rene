0dce893e64bb7f82bb70def6a16a4fb7891b6894
Azat Ibrakov <azatibrakov@gmail.com>
2023-05-19 21:58:38 +0200

    Complete test of 'Contour.locate' method

--- tests/exact_tests/contour_tests/test_locate.py
+++ tests/exact_tests/contour_tests/test_locate.py
@@ -3,6 +3,9 @@ from hypothesis import given
 from rene import Location
 from rene.exact import (Contour,
                         Point)
+from tests.utils import (reverse_contour,
+                         reverse_contour_coordinates,
+                         reverse_point_coordinates)
 from . import strategies
 
 
@@ -18,3 +21,12 @@ def test_vertices(contour: Contour) -> None:
     assert [vertex
             for vertex in contour.vertices
             if contour.locate(vertex) is not Location.BOUNDARY] == []
+
+
+@given(strategies.contours, strategies.points)
+def test_reversals(contour: Contour, point: Point) -> None:
+    assert contour.locate(point) is reverse_contour(contour).locate(point)
+    assert (contour.locate(point)
+            is reverse_contour_coordinates(contour).locate(
+                    reverse_point_coordinates(point)
+            ))
