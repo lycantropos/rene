b0f38111639a7930f90dccad8d678d5acfd91e50
Azat Ibrakov <azatibrakov@gmail.com>
2023-06-06 03:20:07 +0200

    Complete strategies

--- tests/exact_tests/constrained_delaunay_triangulation_tests/strategies.py
+++ tests/exact_tests/constrained_delaunay_triangulation_tests/strategies.py
@@ -1,11 +1,3 @@
-from hypothesis import strategies
+from tests.exact_tests.strategies import polygons
 
-from tests.exact_tests.strategies import (points,
-                                          polygons)
-
-points = points
-points_lists = strategies.lists(points,
-                                min_size=1)
-two_or_more_points_lists = strategies.lists(points,
-                                            min_size=2)
 polygons = polygons
