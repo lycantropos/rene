64ad683d261cb75f8574baff73bf23433bb06535
Azat Ibrakov <azatibrakov@gmail.com>
2023-06-09 01:49:46 +0200

    Complete test of 'Trapezoidation.from_polygon' constructor

--- tests/exact_tests/trapezoidation_tests/test_from_polygon.py
+++ tests/exact_tests/trapezoidation_tests/test_from_polygon.py
@@ -3,6 +3,7 @@ from typing import Any
 import pytest
 from hypothesis import given
 
+from rene import Location
 from rene._exact import (Polygon,
                          Trapezoidation)
 from rene.hints import Seeder
@@ -22,6 +23,29 @@ def test_basic_custom_seeder(polygon: Polygon, seeder: Seeder) -> None:
                                          seeder=seeder)
 
     assert isinstance(result, Trapezoidation)
+
+
+@given(strategies.polygons, strategies.seeders)
+def test_contains(polygon: Polygon, seeder: Seeder) -> None:
+    result = Trapezoidation.from_polygon(polygon,
+                                         seeder=seeder)
+
+    assert all(vertex in result for vertex in polygon.border.vertices)
+    assert all(vertex in result
+               for hole in polygon.holes
+               for vertex in hole.vertices)
+
+
+@given(strategies.polygons, strategies.seeders)
+def test_locate(polygon: Polygon, seeder: Seeder) -> None:
+    result = Trapezoidation.from_polygon(polygon,
+                                         seeder=seeder)
+
+    assert all(result.locate(vertex) is Location.BOUNDARY
+               for vertex in polygon.border.vertices)
+    assert all(result.locate(vertex) is Location.BOUNDARY
+               for hole in polygon.holes
+               for vertex in hole.vertices)
 
 
 @given(strategies.polygons, strategies.invalid_seeds)
