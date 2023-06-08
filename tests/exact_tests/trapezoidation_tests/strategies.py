be2e5d3b4d1ed8783e6fe662dd282aefbb382ecc
Azat Ibrakov <azatibrakov@gmail.com>
2023-06-09 00:03:22 +0200

    Complete test of 'Trapezoidation.from_multisegment' constructor

--- tests/exact_tests/trapezoidation_tests/strategies.py
+++ tests/exact_tests/trapezoidation_tests/strategies.py
@@ -1,6 +1,12 @@
+from hypothesis import strategies as _st
+
 from rene.exact import Trapezoidation
 from tests.exact_tests import strategies as _strategies
 
 multisegments = _strategies.multisegments
 points = _strategies.points
 trapezoidations = multisegments.map(Trapezoidation.from_multisegment)
+seeders = _st.integers(min_value=0).map(lambda seed: (lambda: seed))
+invalid_seeds = (
+        _st.integers(max_value=-1) | _st.floats() | _st.text()
+)
--- tests/exact_tests/trapezoidation_tests/test_from_multisegment.py
+++ tests/exact_tests/trapezoidation_tests/test_from_multisegment.py
@@ -1,12 +1,33 @@
+from typing import Any
+
+import pytest
 from hypothesis import given
 
-from rene.exact import (Multisegment,
+from rene._exact import (Multisegment,
                         Trapezoidation)
+from rene.hints import Seeder
 from . import strategies
 
 
 @given(strategies.multisegments)
-def test_basic(multisegment: Multisegment) -> None:
+def test_basic_default_seeder(multisegment: Multisegment) -> None:
     result = Trapezoidation.from_multisegment(multisegment)
 
     assert isinstance(result, Trapezoidation)
+
+
+@given(strategies.multisegments, strategies.seeders)
+def test_basic_custom_seeder(multisegment: Multisegment,
+                             seeder: Seeder) -> None:
+    result = Trapezoidation.from_multisegment(multisegment,
+                                              seeder=seeder)
+
+    assert isinstance(result, Trapezoidation)
+
+
+@given(strategies.multisegments, strategies.invalid_seeds)
+def test_invalid_seeders(multisegment: Multisegment,
+                         invalid_seed: Any) -> None:
+    with pytest.raises((ValueError, TypeError)):
+        Trapezoidation.from_multisegment(multisegment,
+                                         seeder=lambda: invalid_seed)
