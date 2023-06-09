832ba0990d77b68c504b8b2020f05aa2ff4ba3fb
Azat Ibrakov <azatibrakov@gmail.com>
2023-06-09 03:53:07 +0200

    Add test for 'Contour.__contains__' magic method

---
+++ tests/exact_tests/contour_tests/test__contains__.py
@@ -0,0 +1,21 @@
+from hypothesis import given
+
+from rene import Location
+from rene.exact import (Contour,
+                        Point)
+from tests.utils import equivalence
+from . import strategies
+
+
+@given(strategies.contours, strategies.points)
+def test_basic(contour: Contour, point: Point) -> None:
+    result = point in contour
+
+    assert isinstance(result, bool)
+
+
+@given(strategies.contours, strategies.points)
+def test_alternatives(contour: Contour, point: Point) -> None:
+    result = point in contour
+
+    assert equivalence(result, contour.locate(point) is not Location.EXTERIOR)
