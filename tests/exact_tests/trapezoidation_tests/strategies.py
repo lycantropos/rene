47010681e9d9dd9b2d6c8b43d2a18846b6792e3e
Azat Ibrakov <azatibrakov@gmail.com>
2023-06-09 02:10:22 +0200

    Complete strategies

--- tests/exact_tests/trapezoidation_tests/strategies.py
+++ tests/exact_tests/trapezoidation_tests/strategies.py
@@ -1,14 +1,21 @@
+import sys as _sys
+
 from hypothesis import strategies as _st
 
-from rene.exact import Trapezoidation
+from rene.exact import Trapezoidation as _Trapezoidation
 from tests.exact_tests import strategies as _strategies
+
+_MAX_USIZE_VALUE = (_sys.maxsize << 1) + 1
 
 multisegments = _strategies.multisegments
 points = _strategies.points
 polygons = _strategies.polygons
-trapezoidations = (multisegments.map(Trapezoidation.from_multisegment)
-                   | polygons.map(Trapezoidation.from_polygon))
-seeders = _st.integers(min_value=0).map(lambda seed: (lambda: seed))
-invalid_seeds = (
-        _st.integers(max_value=-1) | _st.floats() | _st.text()
-)
+trapezoidations = (multisegments.map(_Trapezoidation.from_multisegment)
+                   | polygons.map(_Trapezoidation.from_polygon))
+seeders = _st.integers(
+        min_value=0, max_value=_MAX_USIZE_VALUE
+).map(lambda seed: (lambda: seed))
+invalid_seeds = (_st.integers(max_value=-1)
+                 | _st.integers(min_value=_MAX_USIZE_VALUE + 1)
+                 | _st.floats()
+                 | _st.text())
