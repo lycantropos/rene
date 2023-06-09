fd23ce450af62a3c9e61dc403ccafcbb1505676b
Azat Ibrakov <azatibrakov@gmail.com>
2023-06-09 03:53:27 +0200

    Complete test of 'Contour.is_valid' method

--- tests/exact_tests/contour_tests/test_is_valid.py
+++ tests/exact_tests/contour_tests/test_is_valid.py
@@ -3,6 +3,7 @@ from hypothesis import given
 from rene.exact import Contour
 from tests.utils import (equivalence,
                          reverse_contour,
+                         reverse_contour_coordinates,
                          rotate_contour)
 from . import strategies
 
@@ -10,6 +11,12 @@ from . import strategies
 @given(strategies.contours_like)
 def test_basic(contour: Contour) -> None:
     assert isinstance(contour.is_valid(), bool)
+
+
+@given(strategies.contours)
+def test_reversals(contour: Contour) -> None:
+    assert equivalence(contour.is_valid(),
+                       reverse_contour_coordinates(contour).is_valid())
 
 
 @given(strategies.contours, strategies.non_zero_integers)
