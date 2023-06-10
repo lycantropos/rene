b310a2eba836d568443565a82d5efc1d66b69b4a
Azat Ibrakov <azatibrakov@gmail.com>
2023-06-10 09:25:29 +0200

    Complete signatures of utilities

--- rene/_utils.py
+++ rene/_utils.py
@@ -20,7 +20,7 @@ from rene.hints import (Box,
 
 
 class Ordered(_te.Protocol):
-    def __lt__(self, other: _te.Self) -> bool:
+    def __lt__(self, other: _te.Self, /) -> bool:
         ...
 
 
@@ -30,7 +30,7 @@ _T = _t.TypeVar('_T')
 
 
 def are_contour_vertices_non_degenerate(
-        vertices: _t.Sequence[Point[Scalar]]
+        vertices: _t.Sequence[Point[Scalar]], /
 ) -> bool:
     return (all(orient(vertices[index - 1], vertices[index],
                        vertices[index + 1]) is not Orientation.COLLINEAR
@@ -42,17 +42,21 @@ def are_contour_vertices_non_degenerate(
                           is not Orientation.COLLINEAR))))
 
 
-def do_boxes_have_common_area(first: Box[Scalar], second: Box[Scalar]) -> bool:
+def do_boxes_have_common_area(
+        first: Box[Scalar], second: Box[Scalar], /
+) -> bool:
     return not first.disjoint_with(second) and not first.touches(second)
 
 
-def do_boxes_have_no_common_area(first: Box[Scalar],
-                                 second: Box[Scalar]) -> bool:
+def do_boxes_have_no_common_area(
+        first: Box[Scalar], second: Box[Scalar], /
+) -> bool:
     return first.disjoint_with(second) or first.touches(second)
 
 
-def do_boxes_have_common_continuum(first: Box[Scalar],
-                                   second: Box[Scalar]) -> bool:
+def do_boxes_have_common_continuum(
+        first: Box[Scalar], second: Box[Scalar], /
+) -> bool:
     return (not first.disjoint_with(second)
             and
             (not first.touches(second)
@@ -60,8 +64,9 @@ def do_boxes_have_common_continuum(first: Box[Scalar],
              or (first.min_x != second.max_x and second.min_x != first.max_x)))
 
 
-def do_boxes_have_no_common_continuum(first: Box[Scalar],
-                                      second: Box[Scalar]) -> bool:
+def do_boxes_have_no_common_continuum(
+        first: Box[Scalar], second: Box[Scalar], /
+) -> bool:
     return (first.disjoint_with(second)
             or
             (first.touches(second)
@@ -69,14 +74,15 @@ def do_boxes_have_no_common_continuum(first: Box[Scalar],
              and (first.min_x == second.max_x or second.min_x == first.max_x)))
 
 
-def ceil_log2(number: int) -> int:
+def ceil_log2(number: int, /) -> int:
     return number.bit_length() - (not (number & (number - 1)))
 
 
 def collect_maybe_empty_polygons(
         polygons: _t.Sequence[Polygon[Scalar]],
         empty_cls: _t.Type[Empty[Scalar]],
-        multipolygon_cls: _t.Type[Multipolygon[Scalar]]
+        multipolygon_cls: _t.Type[Multipolygon[Scalar]],
+        /
 ) -> _t.Union[Empty[Scalar], Multipolygon[Scalar], Polygon[Scalar]]:
     return (collect_non_empty_polygons(polygons, multipolygon_cls)
             if polygons
@@ -85,7 +91,8 @@ def collect_maybe_empty_polygons(
 
 def collect_non_empty_polygons(
         polygons: _t.Sequence[Polygon[Scalar]],
-        multipolygon_cls: _t.Type[Multipolygon[Scalar]]
+        multipolygon_cls: _t.Type[Multipolygon[Scalar]],
+        /
 ) -> _t.Union[Multipolygon[Scalar], Polygon[Scalar]]:
     assert len(polygons) >= 1
     return polygons[0] if len(polygons) == 1 else multipolygon_cls(polygons)
@@ -94,27 +101,28 @@ def collect_non_empty_polygons(
 def cross_multiply(first_start: Point[Scalar],
                    first_end: Point[Scalar],
                    second_start: Point[Scalar],
-                   second_end: Point[Scalar]) -> Scalar:
+                   second_end: Point[Scalar],
+                   /) -> Scalar:
     return ((first_end.x - first_start.x) * (second_end.y - second_start.y)
             - (first_end.y - first_start.y) * (second_end.x - second_start.x))
 
 
-def deduplicate(values: _t.List[_T]) -> _t.List[_T]:
+def deduplicate(values: _t.List[_T], /) -> _t.List[_T]:
     return [value for value, _ in groupby(values)]
 
 
-def flags_to_false_indices(flags: _t.Sequence[bool]) -> _t.List[int]:
+def flags_to_false_indices(flags: _t.Sequence[bool], /) -> _t.List[int]:
     return [index for index, flag in enumerate(flags) if not flag]
 
 
-def flags_to_true_indices(flags: _t.Sequence[bool]) -> _t.List[int]:
+def flags_to_true_indices(flags: _t.Sequence[bool], /) -> _t.List[int]:
     return [index for index, flag in enumerate(flags) if flag]
 
 
 def intersect_crossing_segments(first_start: Point[Scalar],
                                 first_end: Point[Scalar],
                                 second_start: Point[Scalar],
-                                second_end: Point[Scalar]) -> Point[Scalar]:
+                                second_end: Point[Scalar], /) -> Point[Scalar]:
     scale = (cross_multiply(first_start, second_start, second_start,
                             second_end)
              / cross_multiply(first_start, first_end, second_start,
@@ -125,18 +133,19 @@ def intersect_crossing_segments(first_start: Point[Scalar],
     )
 
 
-def is_even(value: int) -> bool:
+def is_even(value: int, /) -> bool:
     return value & 1 == 0
 
 
-def is_odd(value: int) -> bool:
+def is_odd(value: int, /) -> bool:
     return value & 1 == 1
 
 
 def locate_point_in_point_point_point_circle(point: Point[Scalar],
                                              first: Point[Scalar],
                                              second: Point[Scalar],
-                                             third: Point[Scalar]) -> Location:
+                                             third: Point[Scalar],
+                                             /) -> Location:
     first_dx, first_dy = first.x - point.x, first.y - point.y
     second_dx, second_dy = second.x - point.x, second.y - point.y
     third_dx, third_dy = third.x - point.x, third.y - point.y
@@ -151,8 +160,9 @@ def locate_point_in_point_point_point_circle(point: Point[Scalar],
             else (Location.INTERIOR if raw > 0 else Location.EXTERIOR))
 
 
-def locate_point_in_region(border: Contour[Scalar],
-                           point: Point[Scalar]) -> Location:
+def locate_point_in_region(
+        border: Contour[Scalar], point: Point[Scalar], /
+) -> Location:
     is_point_inside = False
     point_y = point.y
     for edge in border.segments:
@@ -170,7 +180,8 @@ def locate_point_in_region(border: Contour[Scalar],
 
 def locate_point_in_segment(start: Point[Scalar],
                             end: Point[Scalar],
-                            point: Point[Scalar]) -> Location:
+                            point: Point[Scalar],
+                            /) -> Location:
     return (Location.BOUNDARY
             if (start == point
                 or end == point
@@ -184,7 +195,7 @@ def locate_point_in_segment(start: Point[Scalar],
             else Location.EXTERIOR)
 
 
-def merge_boxes(boxes: _t.Iterable[Box[Scalar]]) -> Box[Scalar]:
+def merge_boxes(boxes: _t.Iterable[Box[Scalar]], /) -> Box[Scalar]:
     boxes_iterator = iter(boxes)
     first_box = next(boxes_iterator)
     max_x, min_x = first_box.max_x, first_box.min_x
@@ -203,7 +214,8 @@ def merge_boxes(boxes: _t.Iterable[Box[Scalar]]) -> Box[Scalar]:
 
 def orient(vertex: Point[Scalar],
            first_ray_point: Point[Scalar],
-           second_ray_point: Point[Scalar]) -> Orientation:
+           second_ray_point: Point[Scalar],
+           /) -> Orientation:
     raw = to_sign(cross_multiply(vertex, first_ray_point, vertex,
                                  second_ray_point))
     return (Orientation.COLLINEAR
@@ -214,8 +226,11 @@ def orient(vertex: Point[Scalar],
 
 
 def relate_segments(
-        goal_start: Point[Scalar], goal_end: Point[Scalar],
-        test_start: Point[Scalar], test_end: Point[Scalar]
+        goal_start: Point[Scalar],
+        goal_end: Point[Scalar],
+        test_start: Point[Scalar],
+        test_end: Point[Scalar],
+        /
 ) -> Relation:
     assert goal_start != goal_end
     assert goal_start != goal_end
@@ -286,7 +301,7 @@ def relate_segments(
 
 
 def shrink_collinear_vertices(
-        vertices: _t.Sequence[Point[Scalar]]
+        vertices: _t.Sequence[Point[Scalar]], /
 ) -> _t.List[Point[Scalar]]:
     assert len(vertices) >= MIN_CONTOUR_VERTICES_COUNT
     result = [vertices[0]]
@@ -300,52 +315,56 @@ def shrink_collinear_vertices(
     return result
 
 
-def to_boxes_have_common_area(boxes: _t.Sequence[Box[Scalar]],
-                              target_box: Box[Scalar]) -> _t.List[bool]:
+def to_boxes_have_common_area(
+        boxes: _t.Sequence[Box[Scalar]], target_box: Box[Scalar], /
+) -> _t.List[bool]:
     return [do_boxes_have_common_area(box, target_box) for box in boxes]
 
 
-def to_boxes_have_common_continuum(boxes: _t.Sequence[Box[Scalar]],
-                                   target_box: Box[Scalar]) -> _t.List[bool]:
+def to_boxes_have_common_continuum(
+        boxes: _t.Sequence[Box[Scalar]], target_box: Box[Scalar], /
+) -> _t.List[bool]:
     return [do_boxes_have_common_continuum(box, target_box) for box in boxes]
 
 
-def to_boxes_ids_with_common_area(boxes: _t.Iterable[Box[Scalar]],
-                                  target_box: Box[Scalar]) -> _t.List[int]:
+def to_boxes_ids_with_common_area(
+        boxes: _t.Iterable[Box[Scalar]], target_box: Box[Scalar], /
+) -> _t.List[int]:
     return [box_id
             for box_id, box in enumerate(boxes)
             if do_boxes_have_common_area(box, target_box)]
 
 
 def to_boxes_ids_with_continuous_common_points(
-        boxes: _t.Iterable[Box[Scalar]], target_box: Box[Scalar]
+        boxes: _t.Iterable[Box[Scalar]], target_box: Box[Scalar], /
 ) -> _t.List[int]:
     return [box_id
             for box_id, box in enumerate(boxes)
             if do_boxes_have_common_continuum(box, target_box)]
 
 
-def to_arg_min(values: _t.Sequence[_OrderedT]) -> int:
+def to_arg_min(values: _t.Sequence[_OrderedT], /) -> int:
     return min(range(len(values)),
                key=values.__getitem__)
 
 
-def to_contour_orientation(vertices: _t.Sequence[Point[Scalar]],
-                           min_vertex_index: int) -> Orientation:
+def to_contour_orientation(
+        vertices: _t.Sequence[Point[Scalar]], min_vertex_index: int, /
+) -> Orientation:
     return orient(vertices[min_vertex_index - 1], vertices[min_vertex_index],
                   vertices[(min_vertex_index + 1) % len(vertices)])
 
 
-def to_sign(value: _t.Any) -> int:
+def to_sign(value: _t.Any, /) -> int:
     return 1 if value > 0 else (-1 if value else 0)
 
 
 def to_sorted_pair(first: _OrderedT,
-                   second: _OrderedT) -> _t.Tuple[_OrderedT, _OrderedT]:
+                   second: _OrderedT, /) -> _t.Tuple[_OrderedT, _OrderedT]:
     return (first, second) if first < second else (second, first)
 
 
-def permute(values: _t.MutableSequence[_T], seed: int) -> None:
+def permute(values: _t.MutableSequence[_T], seed: int, /) -> None:
     """
     Based on "Ranking and unranking permutations in linear time"
     by W. Myrvold, F. Ruskey
@@ -361,8 +380,9 @@ def permute(values: _t.MutableSequence[_T], seed: int) -> None:
         seed //= step
 
 
-def validate_seed(seed: _t.Any,
-                  _max_usize_value: int = (_sys.maxsize << 1) + 1) -> None:
+def validate_seed(
+        seed: _t.Any, _max_usize_value: int = (_sys.maxsize << 1) + 1, /
+) -> None:
     error_type: _t.Type[Exception]
     if not isinstance(seed, int):
         error_type = TypeError
