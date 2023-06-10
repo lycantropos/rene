0785ee2fca9192f2b58868b0c2e2acb5a3844735
Azat Ibrakov <azatibrakov@gmail.com>
2023-06-10 14:25:44 +0200

    Complete 'SymmetricDIfference' trait implementation of 'Empty' struct

--- src/geometries/empty/symmetric_difference.rs
+++ src/geometries/empty/symmetric_difference.rs
@@ -1,7 +1,45 @@
-use crate::geometries::{Multipolygon, Polygon};
+use crate::geometries::{Contour, Multipolygon, Multisegment, Polygon, Segment};
 use crate::traits::SymmetricDifference;
 
 use super::types::Empty;
+
+impl<Scalar> SymmetricDifference<Contour<Scalar>> for Empty {
+    type Output = Contour<Scalar>;
+
+    fn symmetric_difference(self, other: Contour<Scalar>) -> Self::Output {
+        other
+    }
+}
+
+impl<Scalar> SymmetricDifference<&Contour<Scalar>> for Empty
+where
+    Contour<Scalar>: Clone,
+{
+    type Output = Contour<Scalar>;
+
+    fn symmetric_difference(self, other: &Contour<Scalar>) -> Self::Output {
+        other.clone()
+    }
+}
+
+impl<Scalar> SymmetricDifference<Contour<Scalar>> for &Empty {
+    type Output = Contour<Scalar>;
+
+    fn symmetric_difference(self, other: Contour<Scalar>) -> Self::Output {
+        other
+    }
+}
+
+impl<Scalar> SymmetricDifference<&Contour<Scalar>> for &Empty
+where
+    Contour<Scalar>: Clone,
+{
+    type Output = Contour<Scalar>;
+
+    fn symmetric_difference(self, other: &Contour<Scalar>) -> Self::Output {
+        other.clone()
+    }
+}
 
 impl SymmetricDifference for Empty {
     type Output = Self;
@@ -32,6 +70,82 @@ impl SymmetricDifference for &Empty {
 
     fn symmetric_difference(self, other: Self) -> Self::Output {
         *other
+    }
+}
+
+impl<Scalar> SymmetricDifference<Multipolygon<Scalar>> for Empty {
+    type Output = Multipolygon<Scalar>;
+
+    fn symmetric_difference(self, other: Multipolygon<Scalar>) -> Self::Output {
+        other
+    }
+}
+
+impl<Scalar> SymmetricDifference<&Multipolygon<Scalar>> for Empty
+where
+    Multipolygon<Scalar>: Clone,
+{
+    type Output = Multipolygon<Scalar>;
+
+    fn symmetric_difference(self, other: &Multipolygon<Scalar>) -> Self::Output {
+        other.clone()
+    }
+}
+
+impl<Scalar> SymmetricDifference<Multipolygon<Scalar>> for &Empty {
+    type Output = Multipolygon<Scalar>;
+
+    fn symmetric_difference(self, other: Multipolygon<Scalar>) -> Self::Output {
+        other
+    }
+}
+
+impl<Scalar> SymmetricDifference<&Multipolygon<Scalar>> for &Empty
+where
+    Multipolygon<Scalar>: Clone,
+{
+    type Output = Multipolygon<Scalar>;
+
+    fn symmetric_difference(self, other: &Multipolygon<Scalar>) -> Self::Output {
+        other.clone()
+    }
+}
+
+impl<Scalar> SymmetricDifference<Multisegment<Scalar>> for Empty {
+    type Output = Multisegment<Scalar>;
+
+    fn symmetric_difference(self, other: Multisegment<Scalar>) -> Self::Output {
+        other
+    }
+}
+
+impl<Scalar> SymmetricDifference<&Multisegment<Scalar>> for Empty
+where
+    Multisegment<Scalar>: Clone,
+{
+    type Output = Multisegment<Scalar>;
+
+    fn symmetric_difference(self, other: &Multisegment<Scalar>) -> Self::Output {
+        other.clone()
+    }
+}
+
+impl<Scalar> SymmetricDifference<Multisegment<Scalar>> for &Empty {
+    type Output = Multisegment<Scalar>;
+
+    fn symmetric_difference(self, other: Multisegment<Scalar>) -> Self::Output {
+        other
+    }
+}
+
+impl<Scalar> SymmetricDifference<&Multisegment<Scalar>> for &Empty
+where
+    Multisegment<Scalar>: Clone,
+{
+    type Output = Multisegment<Scalar>;
+
+    fn symmetric_difference(self, other: &Multisegment<Scalar>) -> Self::Output {
+        other.clone()
     }
 }
 
@@ -73,40 +187,40 @@ where
     }
 }
 
-impl<Scalar> SymmetricDifference<Multipolygon<Scalar>> for Empty {
-    type Output = Multipolygon<Scalar>;
+impl<Scalar> SymmetricDifference<Segment<Scalar>> for Empty {
+    type Output = Segment<Scalar>;
 
-    fn symmetric_difference(self, other: Multipolygon<Scalar>) -> Self::Output {
+    fn symmetric_difference(self, other: Segment<Scalar>) -> Self::Output {
         other
     }
 }
 
-impl<Scalar> SymmetricDifference<&Multipolygon<Scalar>> for Empty
+impl<Scalar> SymmetricDifference<&Segment<Scalar>> for Empty
 where
-    Multipolygon<Scalar>: Clone,
+    Segment<Scalar>: Clone,
 {
-    type Output = Multipolygon<Scalar>;
+    type Output = Segment<Scalar>;
 
-    fn symmetric_difference(self, other: &Multipolygon<Scalar>) -> Self::Output {
+    fn symmetric_difference(self, other: &Segment<Scalar>) -> Self::Output {
         other.clone()
     }
 }
 
-impl<Scalar> SymmetricDifference<Multipolygon<Scalar>> for &Empty {
-    type Output = Multipolygon<Scalar>;
+impl<Scalar> SymmetricDifference<Segment<Scalar>> for &Empty {
+    type Output = Segment<Scalar>;
 
-    fn symmetric_difference(self, other: Multipolygon<Scalar>) -> Self::Output {
+    fn symmetric_difference(self, other: Segment<Scalar>) -> Self::Output {
         other
     }
 }
 
-impl<Scalar> SymmetricDifference<&Multipolygon<Scalar>> for &Empty
+impl<Scalar> SymmetricDifference<&Segment<Scalar>> for &Empty
 where
-    Multipolygon<Scalar>: Clone,
+    Segment<Scalar>: Clone,
 {
-    type Output = Multipolygon<Scalar>;
+    type Output = Segment<Scalar>;
 
-    fn symmetric_difference(self, other: &Multipolygon<Scalar>) -> Self::Output {
+    fn symmetric_difference(self, other: &Segment<Scalar>) -> Self::Output {
         other.clone()
     }
 }
