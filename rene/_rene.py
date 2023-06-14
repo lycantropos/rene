4822545b80ad5324e925d928ca794c0d56fff685
Azat Ibrakov <azatibrakov@gmail.com>
2023-06-14 23:30:20 +0200

    Fix '__repr__' magic method of enumerations

--- rene/_rene.py
+++ rene/_rene.py
@@ -12,10 +12,6 @@ class Base(IntEnum):
     __module__ = 'rene'
 
     def __repr__(self) -> str:
-        return (f'{type(self).__module__}.{type(self).__qualname__}'
-                f'.{self.name}')
-
-    def __str__(self) -> str:
         return f'{type(self).__qualname__}.{self.name}'
 
 
