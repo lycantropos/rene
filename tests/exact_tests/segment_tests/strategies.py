6d3976802deb096a7be864f85576c239ad16e45b
Azat Ibrakov <azatibrakov@gmail.com>
2023-06-24 14:38:36 +0200

    Complete test of 'Segment.relate_to' method

--- tests/exact_tests/segment_tests/strategies.py
+++ tests/exact_tests/segment_tests/strategies.py
@@ -3,3 +3,5 @@ from tests.exact_tests import strategies as _strategies
 points = _strategies.points
 segments_endpoints = _strategies.segments_endpoints
 segments = _strategies.segments
+relatable_compounds = (_strategies.contours | _strategies.multisegments
+                       | _strategies.segments)
