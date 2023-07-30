f17f664057fcfa3cd66ed0a96710d9f25d2b7cdd
Azat Ibrakov <azatibrakov@gmail.com>
2023-07-30 04:03:51 +0200

    Add test for 'Box' class subclassing

---
+++ tests/exact_tests/box_tests/test_subclassing.py
@@ -0,0 +1,22 @@
+import types
+import typing as t
+
+import pytest
+from hypothesis import given
+
+from rene.exact import Box
+from . import strategies
+
+
+def test_static() -> None:
+    with pytest.raises(TypeError):
+        class Subclass(Box):
+            pass
+
+
+@given(strategies.strings, strategies.string_key_dictionaries)
+def test_dynamic(name: str, members: t.Dict[str, t.Any]) -> None:
+    with pytest.raises(TypeError):
+        type(name, (Box,), members)
+    with pytest.raises(TypeError):
+        types.new_class(name, (Box,), members)
