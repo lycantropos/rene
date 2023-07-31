757e6a245a5e55ad9a681f029202a766e09c9545
Azat Ibrakov <azatibrakov@gmail.com>
2023-07-31 05:42:18 +0200

    Complete strategies

--- tests/exact_tests/multisegment_segments_tests/strategies.py
+++ tests/exact_tests/multisegment_segments_tests/strategies.py
@@ -5,7 +5,8 @@ from hypothesis import strategies as _st
 
 from tests.exact_tests import strategies as _strategies
 
-indices = _st.integers(-_sys.maxsize - 1, _sys.maxsize)
+MAX_INDEX = _sys.maxsize >> 1
+indices = _st.integers(-MAX_INDEX, MAX_INDEX)
 non_zero_indices = indices.filter(bool)
 slices = _st.builds(slice, indices, indices, non_zero_indices)
 multisegments_segments = _strategies.multisegments.map(_attrgetter('segments'))
