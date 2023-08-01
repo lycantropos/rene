07b39be31cdce41db1dfc7eab51e906707b28f5d
Azat Ibrakov <azatibrakov@gmail.com>
2023-08-02 00:09:36 +0200

    Fix formatting

--- rene/_hints.py
+++ rene/_hints.py
@@ -3,9 +3,9 @@ import typing as t
 import typing_extensions as te
 
 _Key = t.TypeVar('_Key',
-                  contravariant=True)
+                 contravariant=True)
 _Value = t.TypeVar('_Value',
-                    covariant=True)
+                   covariant=True)
 
 
 class Map(te.Protocol[_Key, _Value]):
