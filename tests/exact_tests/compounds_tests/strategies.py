2c4c2c7366e020c33e08918ec92caec362515dbd
Azat Ibrakov <azatibrakov@gmail.com>
2023-08-02 06:55:25 +0200

    Fix formatting

--- tests/exact_tests/compounds_tests/strategies.py
+++ tests/exact_tests/compounds_tests/strategies.py
@@ -1,6 +1,5 @@
 from tests.exact_tests import strategies as _strategies
 
-compounds = (_strategies.empty_geometries
-             | _strategies.multipolygons
+compounds = (_strategies.empty_geometries | _strategies.multipolygons
              | _strategies.polygons)
 points = _strategies.points
