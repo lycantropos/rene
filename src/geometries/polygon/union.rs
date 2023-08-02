b8e5d4bb221623fe4a1003d017c7dde942e57030
Azat Ibrakov <azatibrakov@gmail.com>
2023-08-02 05:47:07 +0200

    Complete 'Union' trait implementation of 'Polygon' generic struct

--- src/geometries/polygon/union.rs
+++ src/geometries/polygon/union.rs
@@ -2,10 +2,13 @@ use crate::bounded::{Bounded, Box};
 use crate::clipping::shaped::Operation;
 use crate::clipping::traits::ReduceEvents;
 use crate::clipping::{Event, UNION};
-use crate::geometries::{Empty, Point};
-use crate::operations::do_boxes_have_no_common_continuum;
+use crate::geometries::{Empty, Multipolygon, Point};
+use crate::operations::{
+    do_boxes_have_no_common_continuum, flags_to_false_indices,
+    flags_to_true_indices, to_boxes_have_common_continuum,
+};
 use crate::relatable::Relatable;
-use crate::traits::{Elemental, Union};
+use crate::traits::{Elemental, Iterable, Lengthsome, Multipolygonal, Union};
 
 use super::types::Polygon;
 
@@ -81,5 +84,80 @@ where
             events.push(event);
         }
         operation.reduce_events(events)
+    }
+}
+
+impl<Scalar> Union<&Multipolygon<Scalar>> for &Polygon<Scalar>
+where
+    Scalar: Clone + Ord,
+    Operation<Point<Scalar>, UNION>: Iterator<Item = Event>
+        + ReduceEvents<Output = Vec<Polygon<Scalar>>>
+        + for<'a> From<(&'a Polygon<Scalar>, &'a [&'a Polygon<Scalar>])>,
+    Point<Scalar>: Elemental<Coordinate = Scalar>,
+    Polygon<Scalar>: Clone,
+    for<'a> &'a Box<&'a Scalar>: Relatable,
+    for<'a> &'a Multipolygon<Scalar>: Bounded<&'a Scalar>,
+    for<'a> &'a Polygon<Scalar>: Bounded<&'a Scalar>,
+{
+    type Output = Vec<Polygon<Scalar>>;
+
+    fn union(self, other: &Multipolygon<Scalar>) -> Self::Output {
+        let bounding_box = self.to_bounding_box();
+        let other_bounding_box = other.to_bounding_box();
+        let other_polygons = other.polygons();
+        if do_boxes_have_no_common_continuum(
+            &bounding_box,
+            &other_bounding_box,
+        ) {
+            let mut result =
+                other_polygons.into_iter().cloned().collect::<Vec<_>>();
+            result.push(self.clone());
+            return result;
+        }
+        let other_bounding_boxes = other_polygons
+            .iter()
+            .map(Bounded::to_bounding_box)
+            .collect::<Vec<_>>();
+        let other_boxes_have_common_continuum = to_boxes_have_common_continuum(
+            &other_bounding_boxes,
+            &other_bounding_box,
+        );
+        let other_common_continuum_polygons_ids =
+            flags_to_true_indices(&other_boxes_have_common_continuum);
+        if other_common_continuum_polygons_ids.is_empty() {
+            let mut result =
+                other_polygons.into_iter().cloned().collect::<Vec<_>>();
+            result.push(self.clone());
+            return result;
+        }
+        let other_common_continuum_polygons =
+            other_common_continuum_polygons_ids
+                .into_iter()
+                .map(|index| &other_polygons[index])
+                .collect::<Vec<_>>();
+        let mut operation = Operation::<Point<_>, UNION>::from((
+            self,
+            &other_common_continuum_polygons,
+        ));
+        let mut events = {
+            let (_, maybe_events_count) = operation.size_hint();
+            debug_assert!(maybe_events_count.is_some());
+            Vec::with_capacity(unsafe {
+                maybe_events_count.unwrap_unchecked()
+            })
+        };
+        for event in operation.by_ref() {
+            events.push(event);
+        }
+        let mut result = operation.reduce_events(events);
+        result.reserve(
+            other_polygons.len() - other_common_continuum_polygons.len(),
+        );
+        result.extend(
+            flags_to_false_indices(&other_boxes_have_common_continuum)
+                .into_iter()
+                .map(|index| other_polygons[index].clone()),
+        );
+        result
     }
 }
