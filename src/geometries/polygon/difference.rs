84387d334e294475fabab881c7de6eee55901da0
Azat Ibrakov <azatibrakov@gmail.com>
2023-08-02 06:01:27 +0200

    Complete 'Difference' trait implementation of 'Polygon' generic struct

--- src/geometries/polygon/difference.rs
+++ src/geometries/polygon/difference.rs
@@ -2,11 +2,13 @@ use crate::bounded::{Bounded, Box};
 use crate::clipping::shaped::Operation;
 use crate::clipping::traits::ReduceEvents;
 use crate::clipping::{Event, DIFFERENCE};
-use crate::geometries::{Empty, Point};
-use crate::operations::do_boxes_have_no_common_area;
+use crate::geometries::{Empty, Multipolygon, Point};
+use crate::operations::{
+    do_boxes_have_no_common_area, to_boxes_ids_with_common_area,
+};
 use crate::relatable::Relatable;
 use crate::sweeping::traits::EventsContainer;
-use crate::traits::{Difference, Elemental};
+use crate::traits::{Difference, Elemental, Iterable, Multipolygonal};
 
 use super::types::Polygon;
 
@@ -70,6 +72,64 @@ where
         let max_x = bounding_box.get_max_x();
         let mut operation =
             Operation::<Point<_>, DIFFERENCE>::from((self, other));
+        let mut events = {
+            let (_, maybe_events_count) = operation.size_hint();
+            debug_assert!(maybe_events_count.is_some());
+            Vec::with_capacity(unsafe {
+                maybe_events_count.unwrap_unchecked()
+            })
+        };
+        while let Some(event) = operation.next() {
+            if operation.get_event_start(event).x().gt(max_x) {
+                break;
+            }
+            events.push(event);
+        }
+        operation.reduce_events(events)
+    }
+}
+
+impl<Scalar> Difference<&Multipolygon<Scalar>> for &Polygon<Scalar>
+where
+    Scalar: Clone + Ord,
+    Operation<Point<Scalar>, DIFFERENCE>: Iterator<Item = Event>
+        + ReduceEvents<Output = Vec<Polygon<Scalar>>>
+        + for<'a> From<(&'a Polygon<Scalar>, &'a [&'a Polygon<Scalar>])>,
+    Point<Scalar>: Elemental<Coordinate = Scalar>,
+    Polygon<Scalar>: Clone,
+    for<'a> &'a Box<&'a Scalar>: Relatable,
+    for<'a> &'a Multipolygon<Scalar>: Bounded<&'a Scalar>,
+    for<'a> &'a Polygon<Scalar>: Bounded<&'a Scalar>,
+{
+    type Output = Vec<Polygon<Scalar>>;
+
+    fn difference(self, other: &Multipolygon<Scalar>) -> Self::Output {
+        let bounding_box = self.to_bounding_box();
+        let other_bounding_box = other.to_bounding_box();
+        if do_boxes_have_no_common_area(&bounding_box, &other_bounding_box) {
+            return vec![self.clone()];
+        }
+        let other_polygons = other.polygons();
+        let other_bounding_boxes = other_polygons
+            .iter()
+            .map(Bounded::to_bounding_box)
+            .collect::<Vec<_>>();
+        let other_common_area_polygons_ids = to_boxes_ids_with_common_area(
+            &other_bounding_boxes,
+            &other_bounding_box,
+        );
+        if other_common_area_polygons_ids.is_empty() {
+            return vec![self.clone()];
+        }
+        let max_x = bounding_box.get_max_x();
+        let other_common_area_polygons = other_common_area_polygons_ids
+            .into_iter()
+            .map(|index| &other_polygons[index])
+            .collect::<Vec<_>>();
+        let mut operation = Operation::<Point<_>, DIFFERENCE>::from((
+            self,
+            &other_common_area_polygons,
+        ));
         let mut events = {
             let (_, maybe_events_count) = operation.size_hint();
             debug_assert!(maybe_events_count.is_some());
