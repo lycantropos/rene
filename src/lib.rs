fdd594f25076807394d3ec93f53f74fd197adb6f
Azat Ibrakov <azatibrakov@gmail.com>
2023-08-09 22:41:19 +0200

    Complete 'Multisegment.__xor__' magic method

--- src/lib.rs
+++ src/lib.rs
@@ -1801,6 +1801,27 @@ impl PyExactMultisegment {
             Ok(py.NotImplemented())
         }
     }
+
+    fn __xor__(&self, other: &PyAny, py: Python) -> PyResult<PyObject> {
+        if other.is_instance(PyExactEmpty::type_object(py))? {
+            let other = other.extract::<PyRef<PyExactEmpty>>()?;
+            Ok(Self((&self.0).symmetric_difference(&other.0)).into_py(py))
+        } else if other.is_instance(PyExactContour::type_object(py))? {
+            let other = other.extract::<PyRef<PyExactContour>>()?;
+            let segments = (&self.0).symmetric_difference(&other.0);
+            Ok(unpack_maybe_empty_segments(segments, py))
+        } else if other.is_instance(Self::type_object(py))? {
+            let other = other.extract::<PyRef<Self>>()?;
+            let segments = (&self.0).symmetric_difference(&other.0);
+            Ok(unpack_maybe_empty_segments(segments, py))
+        } else if other.is_instance(PyExactSegment::type_object(py))? {
+            let other = other.extract::<PyRef<PyExactSegment>>()?;
+            let segments = (&self.0).symmetric_difference(&other.0);
+            Ok(unpack_maybe_empty_segments(segments, py))
+        } else {
+            Ok(py.NotImplemented())
+        }
+    }
 }
 
 #[pymethods]
