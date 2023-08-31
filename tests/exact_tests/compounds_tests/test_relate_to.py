75175ba3777904b7bf16848423bafdd775d25b9e
Azat Ibrakov <azatibrakov@gmail.com>
2023-09-01 00:48:21 +0200

    Fix 'mypy' errors

--- tests/exact_tests/compounds_tests/test_relate_to.py
+++ tests/exact_tests/compounds_tests/test_relate_to.py
@@ -1,25 +1,28 @@
 from hypothesis import given
 
 from rene import Relation
-from tests.exact_tests.hints import Compound
+from tests.exact_tests.hints import MaybeLinearCompound
 from tests.utils import reverse_compound_coordinates
 from . import strategies
 
 
 @given(strategies.maybe_linear_compounds, strategies.maybe_linear_compounds)
-def test_basic(first: Compound, second: Compound) -> None:
+def test_basic(first: MaybeLinearCompound,
+               second: MaybeLinearCompound) -> None:
     result = first.relate_to(second)
 
     assert isinstance(result, Relation)
 
 
 @given(strategies.maybe_linear_compounds, strategies.maybe_linear_compounds)
-def test_complement(first: Compound, second: Compound) -> None:
+def test_complement(first: MaybeLinearCompound,
+                    second: MaybeLinearCompound) -> None:
     assert first.relate_to(second) is second.relate_to(first).complement
 
 
 @given(strategies.maybe_linear_compounds, strategies.maybe_linear_compounds)
-def test_reversals(first: Compound, second: Compound) -> None:
+def test_reversals(first: MaybeLinearCompound,
+                   second: MaybeLinearCompound) -> None:
     assert (first.relate_to(second)
             is reverse_compound_coordinates(first).relate_to(
                     reverse_compound_coordinates(second)
