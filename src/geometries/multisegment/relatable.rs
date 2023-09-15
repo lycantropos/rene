d67b8d023219944a3664a547e32c206fb440a090
Azat Ibrakov <azatibrakov@gmail.com>
2023-09-15 02:07:15 +0200

    Complete 'Relatable' trait implementation of 'Multisegment' generic struct

--- src/geometries/multisegment/relatable.rs
+++ src/geometries/multisegment/relatable.rs
@@ -3,16 +3,20 @@ use std::ops::{Div, Neg};
 
 use traiter::numbers::Signed;
 
-use crate::geometries::{Contour, Empty, Point, Segment};
+use crate::bounded;
+use crate::bounded::Bounded;
+use crate::geometries::{Contour, Empty, Point, Polygon, Segment};
 use crate::operations::{
     CrossMultiply, DotMultiply, IntersectCrossingSegments, Orient, Square,
     SquaredMetric,
 };
 use crate::relatable::{Relatable, Relation};
-use crate::relating::{linear, multisegment, Event};
+use crate::relating::{linear, mixed, multisegment, Event};
 use crate::sweeping::traits::{EventsQueue, SweepLine};
 use crate::traits::{
-    Elemental, Multisegmental, MultisegmentalIndexSegment, Segmental,
+    Contoural, Elemental, Multisegmental, MultisegmentalIndexSegment,
+    Multivertexal, MultivertexalIndexVertex, Polygonal,
+    PolygonalIntoIteratorHole, Segmental,
 };
 
 use super::types::Multisegment;
@@ -76,6 +80,46 @@ where
 {
     fn relate_to(self, other: &Contour<Scalar>) -> Relation {
         multisegment::relate_to_contour(self, other)
+    }
+}
+
+impl<
+        Scalar: Div<Output = Scalar>
+            + Hash
+            + Neg<Output = Scalar>
+            + Ord
+            + Square<Output = Scalar>,
+    > Relatable<&Polygon<Scalar>> for &Multisegment<Scalar>
+where
+    Point<Scalar>: Clone + Hash + Ord,
+    Segment<Scalar>: Clone + Segmental<Endpoint = Point<Scalar>>,
+    mixed::Operation<true, false, Point<Scalar>>:
+        EventsQueue<Event = Event> + SweepLine<Event = Event>,
+    mixed::Operation<true, true, Point<Scalar>>:
+        EventsQueue<Event = Event> + SweepLine<Event = Event>,
+    for<'a, 'b> &'a <PolygonalIntoIteratorHole<&'b Polygon<Scalar>> as Multisegmental>::IndexSegment: Segmental,
+    for<'a, 'b> &'a <PolygonalIntoIteratorHole<&'b Polygon<Scalar>> as Multivertexal>::IndexVertex: Elemental,
+    for<'a, 'b> &'a MultivertexalIndexVertex<&'b Contour<Scalar>>: Elemental,
+    for<'a, 'b> &'a bounded::Box<&'b Scalar>: Relatable,
+    for<'a, 'b> linear::Operation<Point<Scalar>>: From<(&'a [&'b Segment<Scalar>], &'a [&'b Segment<Scalar>])>
+        + EventsQueue<Event = Event>
+        + SweepLine<Event = Event>,
+    for<'a> &'a Contour<Scalar>: Bounded<&'a Scalar>
+        + Contoural<IndexSegment = Segment<Scalar>, IntoIteratorSegment = &'a Segment<Scalar>>,
+    for<'a> &'a Multisegment<Scalar>:
+        Bounded<&'a Scalar> + Multisegmental<IndexSegment = Segment<Scalar>>,
+    for<'a> &'a Point<Scalar>: CrossMultiply<Output = Scalar>
+        + DotMultiply<Output = Scalar>
+        + Elemental<Coordinate = &'a Scalar>
+        + IntersectCrossingSegments<Output = Point<Scalar>>
+        + Orient
+        + SquaredMetric<Output = Scalar>,
+    for<'a> &'a Polygon<Scalar>: Polygonal<Contour = &'a Contour<Scalar>, IndexHole = Contour<Scalar>>,
+    for<'a> &'a Scalar: Signed,
+    for<'a> &'a Segment<Scalar>: Bounded<&'a Scalar> + Segmental<Endpoint = &'a Point<Scalar>>,
+{
+    fn relate_to(self, other: &Polygon<Scalar>) -> Relation {
+        multisegment::relate_to_polygon(self, other)
     }
 }
 
