f19c1f13e48f4fbecd06693755252a04411c1ef7
Azat Ibrakov <azatibrakov@gmail.com>
2023-09-19 02:01:49 +0200

    Complete 'Relatable' trait implementation of 'Segment' generic struct

--- src/geometries/segment/relatable.rs
+++ src/geometries/segment/relatable.rs
@@ -2,14 +2,18 @@ use crate::bounded::Bounded;
 use std::hash::Hash;
 use std::ops::Div;
 
-use crate::geometries::{Contour, Empty, Multisegment, Point, Polygon};
+use crate::geometries::{
+    Contour, Empty, Multipolygon, Multisegment, Point, Polygon,
+};
 use crate::operations::{CrossMultiply, IntersectCrossingSegments, Orient};
 use crate::oriented::Oriented;
 use crate::relatable::{Relatable, Relation};
-use crate::relating::segment;
+use crate::relating::{mixed, segment, Event};
+use crate::sweeping::traits::{EventsQueue, SweepLine};
 use crate::traits::{
-    Contoural, Elemental, Multisegmental, MultisegmentalIndexSegment,
-    Multivertexal, MultivertexalIndexVertex, Polygonal,
+    Contoural, Elemental, Multipolygonal, MultipolygonalIntoIteratorPolygon,
+    Multisegmental, MultisegmentalIndexSegment, Multivertexal,
+    MultivertexalIndexVertex, Polygonal, PolygonalContour, PolygonalIndexHole,
     PolygonalIntoIteratorHole, Segmental,
 };
 
@@ -47,6 +51,73 @@ where
 {
     fn relate_to(self, other: &Contour<Scalar>) -> Relation {
         segment::relate_to_contour(self, other)
+    }
+}
+
+impl<Scalar: Ord> Relatable<&Multipolygon<Scalar>> for &Segment<Scalar>
+where
+    mixed::Operation<true, Point<Scalar>>:
+        EventsQueue<Event = Event> + SweepLine<Event = Event>,
+    Point<Scalar>: Clone + Ord,
+    Segment<Scalar>: Clone + Segmental<Endpoint = Point<Scalar>>,
+    for<'a> &'a Contour<Scalar>: Bounded<&'a Scalar>
+        + Contoural<IntoIteratorSegment = &'a Segment<Scalar>>,
+    for<'a> &'a Multipolygon<Scalar>:
+        Multipolygonal<IndexPolygon = Polygon<Scalar>>,
+    for<'a> &'a Point<Scalar>: Elemental<Coordinate = &'a Scalar>
+        + IntersectCrossingSegments<Output = Point<Scalar>>
+        + Orient,
+    for<'a> &'a Polygon<Scalar>: Bounded<&'a Scalar>
+        + Polygonal<
+            Contour = &'a Contour<Scalar>,
+            IntoIteratorHole = &'a Contour<Scalar>,
+        >,
+    for<'a> &'a Segment<Scalar>:
+        Bounded<&'a Scalar> + Segmental<Endpoint = &'a Point<Scalar>>,
+    for<'a, 'b> &'a MultisegmentalIndexSegment<
+        PolygonalContour<
+            MultipolygonalIntoIteratorPolygon<&'b Multipolygon<Scalar>>,
+        >,
+    >: Segmental,
+    for<'a, 'b> &'a MultisegmentalIndexSegment<
+        PolygonalIntoIteratorHole<
+            MultipolygonalIntoIteratorPolygon<&'b Multipolygon<Scalar>>,
+        >,
+    >: Segmental,
+    for<'a, 'b> &'a MultivertexalIndexVertex<
+        PolygonalContour<
+            MultipolygonalIntoIteratorPolygon<&'b Multipolygon<Scalar>>,
+        >,
+    >: Elemental,
+    for<'a, 'b> &'a MultivertexalIndexVertex<
+        PolygonalIntoIteratorHole<
+            MultipolygonalIntoIteratorPolygon<&'b Multipolygon<Scalar>>,
+        >,
+    >: Elemental,
+    for<'a, 'b> &'a MultisegmentalIndexSegment<&'b Contour<Scalar>>: Segmental,
+    for<'a, 'b> &'a MultivertexalIndexVertex<&'b Contour<Scalar>>: Elemental,
+    for<'a, 'b> &'a PolygonalIndexHole<
+        MultipolygonalIntoIteratorPolygon<&'b Multipolygon<Scalar>>,
+    >: Contoural,
+    for<'a, 'b> &'a PolygonalIndexHole<&'b Polygon<Scalar>>: Contoural,
+    for<'a, 'b, 'c> &'a MultisegmentalIndexSegment<
+        &'b PolygonalIndexHole<
+            MultipolygonalIntoIteratorPolygon<&'c Multipolygon<Scalar>>,
+        >,
+    >: Segmental,
+    for<'a, 'b, 'c> &'a MultivertexalIndexVertex<
+        &'b PolygonalIndexHole<
+            MultipolygonalIntoIteratorPolygon<&'c Multipolygon<Scalar>>,
+        >,
+    >: Elemental,
+    for<'a, 'b, 'c> &'a MultisegmentalIndexSegment<
+        &'b PolygonalIndexHole<&'c Polygon<Scalar>>,
+    >: Segmental,
+    for<'a, 'b, 'c> &'a MultivertexalIndexVertex<&'b PolygonalIndexHole<&'c Polygon<Scalar>>>:
+        Elemental,
+{
+    fn relate_to(self, other: &Multipolygon<Scalar>) -> Relation {
+        segment::relate_to_multipolygon(self, other)
     }
 }
 
