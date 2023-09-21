6dd2a36998b0f90479a948128e3a8581afbf45ac
Azat Ibrakov <azatibrakov@gmail.com>
2023-09-21 02:15:10 +0200

    Complete test of 'relate_to' methods

--- tests/exact_tests/compounds_tests/test_relate_to.py
+++ tests/exact_tests/compounds_tests/test_relate_to.py
@@ -1,14 +1,14 @@
 from hypothesis import given
 
 from rene import Relation
-from tests.exact_tests.hints import MaybeLinearCompound
+from tests.exact_tests.hints import (Compound,
+                                     MaybeLinearCompound)
 from tests.utils import reverse_compound_coordinates
 from . import strategies
 
 
-@given(strategies.maybe_linear_compounds, strategies.maybe_linear_compounds)
-def test_basic(first: MaybeLinearCompound,
-               second: MaybeLinearCompound) -> None:
+@given(strategies.maybe_linear_compounds, strategies.compounds)
+def test_basic(first: MaybeLinearCompound, second: Compound) -> None:
     result = first.relate_to(second)
 
     assert isinstance(result, Relation)
@@ -20,9 +20,8 @@ def test_complement(first: MaybeLinearCompound,
     assert first.relate_to(second) is second.relate_to(first).complement
 
 
-@given(strategies.maybe_linear_compounds, strategies.maybe_linear_compounds)
-def test_reversals(first: MaybeLinearCompound,
-                   second: MaybeLinearCompound) -> None:
+@given(strategies.maybe_linear_compounds, strategies.compounds)
+def test_reversals(first: MaybeLinearCompound, second: Compound) -> None:
     assert (first.relate_to(second)
             is reverse_compound_coordinates(first).relate_to(
                     reverse_compound_coordinates(second)
