f3978d6bf6d301ce0af3411e72768a2aab98d66d
Azat Ibrakov <azatibrakov@gmail.com>
2023-10-03 23:37:08 +0200

    Add 'relate_to_multipolygon' function

--- rene/_relating/multipolygon.py
+++ rene/_relating/multipolygon.py
@@ -6,13 +6,70 @@ from itertools import chain
 from rene import (Relation,
                   hints)
 from rene._utils import to_boxes_ids_with_intersection
-from . import mixed
+from . import (mixed,
+               shaped)
+from .utils import polygon_to_segments
 
 
 def relate_to_contour(multipolygon: hints.Multipolygon[hints.Scalar],
                       contour: hints.Contour[hints.Scalar],
                       /) -> Relation:
     return relate_to_multisegmental(multipolygon, contour)
+
+
+def relate_to_multipolygon(first: hints.Multipolygon[hints.Scalar],
+                           second: hints.Multipolygon[hints.Scalar],
+                           /) -> Relation:
+    first_bounding_box, second_bounding_box = (first.bounding_box,
+                                               second.bounding_box)
+    if first_bounding_box.disjoint_with(second_bounding_box):
+        return Relation.DISJOINT
+    first_polygons = first.polygons
+    first_boxes = [polygon.bounding_box for polygon in first_polygons]
+    first_intersecting_polygons_ids = to_boxes_ids_with_intersection(
+            first_boxes, second_bounding_box
+    )
+    if not first_intersecting_polygons_ids:
+        return Relation.DISJOINT
+    second_polygons = second.polygons
+    second_boxes = [polygon.bounding_box for polygon in second_polygons]
+    second_intersecting_polygons_ids = to_boxes_ids_with_intersection(
+            second_boxes, first_bounding_box
+    )
+    if not second_intersecting_polygons_ids:
+        return Relation.DISJOINT
+    min_max_x = min(
+            max(first_boxes[polygon_id].max_x
+                for polygon_id in first_intersecting_polygons_ids),
+            max(second_boxes[polygon_id].max_x
+                for polygon_id in second_intersecting_polygons_ids)
+    )
+    first_intersecting_polygons = [
+        first_polygons[polygon_id]
+        for polygon_id in first_intersecting_polygons_ids
+        if first_boxes[polygon_id].min_x <= min_max_x
+    ]
+    if not first_intersecting_polygons:
+        return Relation.DISJOINT
+    second_intersecting_polygons = [
+        second_polygons[polygon_id]
+        for polygon_id in second_intersecting_polygons_ids
+        if second_boxes[polygon_id].min_x <= min_max_x
+    ]
+    if not second_intersecting_polygons:
+        return Relation.DISJOINT
+    return shaped.Operation.from_segments_iterables(
+            chain.from_iterable(polygon_to_segments(polygon,
+                                                    second_bounding_box)
+                                for polygon in first_intersecting_polygons),
+            chain.from_iterable(polygon_to_segments(polygon,
+                                                    first_bounding_box)
+                                for polygon in second_intersecting_polygons)
+    ).to_relation(
+            len(first_intersecting_polygons) == len(first_polygons),
+            len(second_intersecting_polygons) == len(second_polygons),
+            min_max_x
+    )
 
 
 def relate_to_multisegment(multipolygon: hints.Multipolygon[hints.Scalar],
---
+++ rene/_relating/utils.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+import typing as t
+from itertools import chain
+
+from rene import hints
+
+
+def polygon_to_segments(polygon: hints.Polygon[hints.Scalar],
+                        bounding_box: hints.Box[hints.Scalar],
+                        /) -> t.Iterable[hints.Segment[hints.Scalar]]:
+    return chain(
+            polygon.border.segments,
+            chain.from_iterable(
+                    hole.segments
+                    for hole in polygon.holes
+                    if not hole.bounding_box.disjoint_with(bounding_box)
+            )
+    )
