5fdc4bee8c45ae9bbb877c503d0ab3e05b821171
Azat Ibrakov <azatibrakov@gmail.com>
2023-10-07 02:40:40 +0200

    Complete setup script

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import platform
+import sys
 
 from setuptools import (find_packages,
                         setup)
@@ -7,7 +7,7 @@ project_base_url = 'https://github.com/lycantropos/rene/'
 parameters = dict(packages=find_packages(exclude=('tests', 'tests.*')),
                   url=project_base_url,
                   download_url=project_base_url + 'archive/master.zip')
-if platform.python_implementation() == 'CPython':
+if sys.implementation.name == 'cpython':
     from setuptools_rust import RustExtension
 
     parameters.update(rust_extensions=[RustExtension('rene._cexact'),
