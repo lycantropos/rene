d0106950ce686f5a50951701b6aa59726f460b3c
Azat Ibrakov <azatibrakov@gmail.com>
2023-10-25 21:55:17 +0200

    Fix strategies

--- tests/exact_tests/box_tests/strategies.py
+++ tests/exact_tests/box_tests/strategies.py
@@ -3,7 +3,7 @@ from hypothesis import strategies as _st
 from rene.exact import Box as _Box
 from tests.exact_tests import strategies as _strategies
 
-strings = _st.text()
+strings = _st.text(_st.characters(blacklist_categories=('Cc', 'Cs')))
 string_key_dictionaries = _st.dictionaries(strings, _st.from_type(object))
 non_zero_integers = _strategies.non_zero_integers
 boxes_limits = _strategies.boxes_limits
