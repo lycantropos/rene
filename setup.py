b004c6c4e7723eebacf80cc6ba398b9a0ad512a8
Azat Ibrakov <azatibrakov@gmail.com>
2024-11-12 10:12:13 +0100

    Complete setup script

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
-import sys
+from __future__ import annotations
 
-from setuptools import (find_packages,
-                        setup)
+import sys
+from typing import Any
+
+from setuptools import find_packages, setup
 
 project_base_url = 'https://github.com/lycantropos/rene/'
-parameters = dict(packages=find_packages(exclude=('tests', 'tests.*')),
-                  url=project_base_url,
-                  download_url=project_base_url + 'archive/master.zip')
+parameters: dict[str, Any] = dict(
+    packages=find_packages(exclude=('tests', 'tests.*')),
+    url=project_base_url,
+    download_url=project_base_url + 'archive/master.zip',
+)
 if sys.implementation.name == 'cpython':
     from setuptools_rust import RustExtension
 
-    parameters.update(rust_extensions=[RustExtension('rene._cexact'),
-                                       RustExtension('rene._crene')],
-                      zip_safe=False)
+    parameters.update(
+        rust_extensions=[
+            RustExtension('rene._cexact'),
+            RustExtension('rene._crene'),
+        ],
+        zip_safe=False,
+    )
 setup(**parameters)
