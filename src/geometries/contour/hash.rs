6110a940f88d6979ad394513b4d3649e9f0b81b3
Azat Ibrakov <azatibrakov@gmail.com>
2026-09-04 00:16:08 +0300

    Complete "Hash" trait implementation for "Contour" generic struct

--- src/geometries/contour/hash.rs
+++ src/geometries/contour/hash.rs
@@ -12,18 +12,19 @@ where
         let min_vertex_index = self.to_min_vertex_index();
         self.vertices[min_vertex_index].hash(state);
         if self.to_orientation() == Orientation::Clockwise {
-            for index in (0..min_vertex_index).rev() {
-                self.vertices[index].hash(state);
+            for vertex in self.vertices.iter().take(min_vertex_index).rev() {
+                vertex.hash(state);
             }
-            for index in (min_vertex_index + 1..self.vertices.len()).rev() {
-                self.vertices[index].hash(state);
+            for vertex in self.vertices.iter().skip(min_vertex_index + 1).rev()
+            {
+                vertex.hash(state);
             }
         } else {
-            for index in min_vertex_index + 1..self.vertices.len() {
-                self.vertices[index].hash(state);
+            for vertex in self.vertices.iter().skip(min_vertex_index + 1) {
+                vertex.hash(state);
             }
-            for index in 0..min_vertex_index {
-                self.vertices[index].hash(state);
+            for vertex in self.vertices.iter().take(min_vertex_index) {
+                vertex.hash(state);
             }
         }
     }
